f99e6af130ee80669bf7ee27106b5da3474f7a36
jackpaulcollins <j.collins@guideline.com>
2023-06-27 20:21:10 -0700

    wrap challenge creation

--- db/seeds/challenges.seeds.rb
+++ db/seeds/challenges.seeds.rb
@@ -1,18 +1,25 @@
 Account.all.each do |account|
   rand(5..25).times do
     start_date = Faker::Date.between(from: 3.months.ago, to: Date.today + 3.months)
-    params = {
+    public_challenge = rand(4).zero? ? true : false
+    challenge = Challenge.new(
       name: Faker::Name.first_name,
       start_date: start_date,
       # 50% shot of being ongoing
       end_date: rand(2).zero? ? Faker::Date.between(from: start_date + 1.week, to: start_date + 1.month) : nil,
       # 25% shot of being public
-      is_public_challenge: rand(4).zero? ? true : false,
+      is_public_challenge: public_challenge,
       account_id: account.id,
       challenge_owner_id: account.owner.id,
-    }
-    challenge = Challenge.create!(params)
+    )
 
+    if public_challenge
+      ActsAsTenant.with_tenant(Account.default_account) do
+        challenge.save!
+      end
+    else
+      challenge.save!
+    end
     # create the challenge units now too
 
     rand(1..5).times do
