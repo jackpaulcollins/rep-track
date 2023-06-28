1989abe1e2aab9e051bbc9c3b1b3f3675c15e03a
jackpaulcollins <j.collins@guideline.com>
2023-06-27 18:07:13 -0700

    output sql during seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,13 +2,12 @@ require 'faker'
 
 def load_development_seeds(name)
   ActiveRecord::Base.transaction do
-    ActiveRecord::Base.logger.level = :debug # Enable SQL logging
     load(File.join(Rails.root, "db", "seeds", "#{name}.seeds.rb"))
-    ActiveRecord::Base.logger.level = :info # Reset logger level after seed execution
   end
 end
 
 begin
+  ActiveRecord::Base.logger = Logger.new(STDOUT)
   load_development_seeds("accounts")
   load_development_seeds("users")
   load_development_seeds("challenges")
@@ -16,4 +15,6 @@ begin
 rescue StandardError => e
   puts "An error occurred: #{e.message}"
   puts e.backtrace.join("\n")
+ensure
+  ActiveRecord::Base.logger = nil
 end
