5731d02fbef540d619fc39cc5672cf803ac6c6e8
Jack Collins <jackcollins@Jacks-MacBook-Pro.local>
2023-07-10 22:53:08 -0700

    stuff

--- db/seeds/reports.seeds.rb
+++ db/seeds/reports.seeds.rb
@@ -1,10 +1,31 @@
 Challenge.all.each do |challenge|
-  enrollment = challenge.challenge_enrollments.all.sample
-  challenge.reports.build(
-    account: challenge.account,
-    user: enrollment.user,
-    challenge_enrollment: enrollment,
-    challenge_unit: challenge.challenge_units.all.sample,
-    rep_count: rand(1..100)
-  ).save!
+  if challenge.end_date.present?
+    (challenge.start_date..challenge.end_date).each do |day|
+      challenge.challenge_enrollments.each do |enrollment|
+        report = challenge.reports.new(
+          account: challenge.account,
+          user: enrollment.user,
+          challenge_enrollment: enrollment,
+          challenge_unit: challenge.challenge_units.all.sample,
+          rep_count: rand(1..100)
+        )
+        report.save!
+        report.update_columns(report_date: day)
+      end
+    end
+  else
+    (challenge.start_date..challenge.start_date + rand(10..15).days).each do |day|
+      challenge.challenge_enrollments.each do |enrollment|
+        report = challenge.reports.new(
+          account: challenge.account,
+          user: enrollment.user,
+          challenge_enrollment: enrollment,
+          challenge_unit: challenge.challenge_units.all.sample,
+          rep_count: rand(1..100)
+        )
+        report.save!
+        report.update_columns(report_date: day)
+      end
+    end
+  end
 end
