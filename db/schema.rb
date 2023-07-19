0e202963894825329eb7b96a21260d9092a49045
jackpaulcollins <j.collins@guideline.com>
2023-07-19 15:53:38 -0700

    improve styling for rep addition

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,6 @@
 
 ActiveRecord::Schema[7.0].define(version: 2023_07_18_001028) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "pg_stat_statements"
   enable_extension "plpgsql"
 
   create_table "account_invitations", force: :cascade do |t|
